5a8bb2432cfc325db27b11968b8da33003633dc6
Stujo <stujo@users.noreply.github.com>
2019-05-08 15:03:29 -0700

    Add colorize

--- Gemfile
+++ Gemfile
@@ -12,3 +12,6 @@ gem "rspec", "~> 3.8"
 
 # Added at 2019-05-08 13:47:17 -0700 by stuart:
 gem "simplecov", "~> 0.16.1"
+
+# Added at 2019-05-08 15:02:35 -0700 by stuart:
+gem "colorize", "~> 0.8.1"
--- lib/application.rb
+++ lib/application.rb
@@ -1,11 +1,13 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), "./lib")
 
+require 'colorize'
+
 class Application
   def self.boot
     self.new.run
   end
   def run
-    puts "Hello World"
+    puts "Hello World".yellow
   end
 end
 
